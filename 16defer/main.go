bbc5db9ca015407ac23cadfea695d1d0b751d44f
enigma0884 <sohanshashi0884@gmail.com>
2022-10-17 00:53:36 +0530

    defer

---
+++ 16defer/main.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+	defer fmt.Println("One")
+	defer fmt.Println("Two")
+	// LIFO: Last in first out
+
+	fmt.Println("Hello")
+	myDefer()
+}
+
+func myDefer() {
+	for i := 0; i < 5; i++ {
+		defer fmt.Println(i)
+	}
+}
+
+/*
+	Output:
+	Hello
+	4
+	3
+	2
+	1
+	0
+	Two
+	One
+
+	Queue:
+	One, Two, 0, 1, 2, 3, 4
+*/
