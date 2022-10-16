b0380a6f49fb5171be08391ef3e7449e874acc2d
enigma0884 <sohanshashi0884@gmail.com>
2022-10-17 00:35:51 +0530

    methods

---
+++ 15methods/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	sohan := User{"sohan", "sohanshashi0884@gmail.com", true, 19}
+	sohan.GetStatus()
+	sohan.ChangeEmail("shashisohan0884@gmail.com")
+	fmt.Println(sohan.Email) // does not change, exactly why pointers come to use
+}
+
+type User struct {
+	Name     string
+	Email    string
+	Verified bool
+	Age      int
+}
+
+func (u User) GetStatus() {
+	fmt.Printf("Is %v verified? -> %v\n", u.Name, u.Verified)
+}
+
+func (u User) ChangeEmail(newMail string) {
+	u.Email = newMail
+	fmt.Printf("Email successfully changed to %v\n", u.Email)
+}
