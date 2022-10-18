c15dd90efc634b4ab8e6b8054906556cb1cf52b7
enigma0884 <sohanshashi0884@gmail.com>
2022-10-18 21:42:10 +0530

    post form data

--- 20webrequests/main.go
+++ 20webrequests/main.go
@@ -4,12 +4,14 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"net/url"
 	"strings"
 )
 
 func main() {
 	// Getdata("http://localhost:1111/get")
-	PostData("http://localhost:1111/post")
+	// PostData("http://localhost:1111/post")
+	PostFormData("http://localhost:1111/postform")
 }
 
 func Getdata(url string) {
@@ -39,6 +41,29 @@ func PostData(url string) {
 	`)
 
 	response, err := http.Post(url, "application/json", requestBody)
+	if err != nil {
+		panic(err)
+	}
+
+	defer response.Body.Close()
+
+	data, err := io.ReadAll(response.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(data))
+}
+
+func PostFormData(payloadURL string) {
+	formData := url.Values{}
+
+	formData.Add("name", "Sohan")
+	formData.Add("surname", "Shashikumar")
+	formData.Add("role", "fullstack developer")
+	formData.Add("ID", "69420")
+
+	response, err := http.PostForm(payloadURL, formData)
 	if err != nil {
 		panic(err)
 	}
